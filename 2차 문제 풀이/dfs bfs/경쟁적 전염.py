2b92f7d60be0cde4fc2c41788d7ea76a7d105e77
won-jae-jang <dnjswo410@naver.com>
2024-11-26 11:46:30 +0900

    11/26 dfs 문제풀이

--- 2차 문제 풀이/dfs bfs/경쟁적 전염.py
+++ 2차 문제 풀이/dfs bfs/경쟁적 전염.py
@@ -9,35 +9,35 @@ s, x, y = map(int, input().split()) #s초가 지난 후에 x, y에 존재하는 
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-#todo 
-#바이러스 크기가 작은 순서대로 큐에 삽입 -> (type, (x, y)) sort 
-#시간 측정 ? -> 큐에 함께 삽입
+#바이러스 크기가 작은 순서대로 큐에 삽입 -> (type, (x, y), time) sort 
 def bfs(graph):
     temp = []
     for i in range(n):
         for j in range(n):
             if graph[i][j] != 0:
-                temp.append([graph[i][j], [i, j]])
+                temp.append((graph[i][j], (i, j), 0))
     temp.sort() #초기의 바이러스 전염 순서
-    q = deque(temp) # [(1, 2), (2, 3) ..]
-    print('q: ',q)
-    time = 0
-    while True:
-        if time == s:
-            print(graph[x - 1][y - 1])
-            break
+    q = deque(temp) # [(type, (1, 2), time), (type, (2, 3), time) ..]
+    # print('q: ',q)
         
-        temp = []
-        while q:
-            x, y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx >= 0 and nx < n and ny >= 0 and ny < n:
-                    if graph[nx][ny] == 0:
-                        graph[nx][ny] = graph[x][y]
-                        temp.append([nx, ny])
-        time += 1
-        q.append(temp)
+    while q:
+        type, pos, time = q.popleft()
+        # print('pop: ', type, pos, time)
+        vx, vy = pos[0], pos[1] #현재 바이러스 위치
+        # print(x, y, vx, vy, time, s)
+        if x == vx + 1 and y == vy + 1:
+            if time <= s:
+                return type
+            else:
+                return 0
+        #바이러스 전이
+        for i in range(4):
+            nx = vx + dx[i]
+            ny = vy + dy[i]
+            if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = type
+                    q.append((type, (nx, ny), time + 1))
+                    # print('input: ', (type, (nx, ny), time + 1))
 
-bfs(graph)
+print(bfs(graph))
--- 2차 문제 풀이/메모장.py
+++ 2차 문제 풀이/메모장.py
@@ -1,6 +1,5 @@
-x, y = 1, 2
-dx, dy = x, y
-
-dx = 3
-dy = 3
-print(x, y)
+a = []
+a.append({(1, 2), (2, 3)})
+a.append({(2, 3), (1, 2)})
+# print(a[0] == a[1])
+print(a[0])
