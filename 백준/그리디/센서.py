a3ba4313d65d7fede52aaa8b8b78bc5aea4bb86f
won-jae-jang <dnjswo410@naver.com>
2025-01-13 19:58:02 +0900

    01/12 백준 문풀

--- 백준/그리디/센서.py
+++ 백준/그리디/센서.py
@@ -1,3 +1,11 @@
+n = int(input())
+k = int(input())
 data = list(map(int, input().split()))
 data.sort()
-print(data)
+
+dist = [] #센서와 센서 사이의 거리
+for i in range(n - 1):
+    dist.append(data[i + 1] - data[i])
+
+dist.sort()
+print(sum(dist[:n - k]))
--- 백준/메모장.py
+++ 백준/메모장.py
@@ -1,2 +1,1 @@
-for i in range(3, 1, -1):
-    print(i)
+lst = [1, 2, 3]
